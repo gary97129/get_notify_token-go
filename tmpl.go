38a74a89853c8f95b6bfc92f03b45abd5013439d
gary97129 <79617638+gary97129@users.noreply.github.com>
2021-12-24 05:23:34 +0800

    Update tmpl.go

--- tmpl.go
+++ tmpl.go
@@ -23,3 +23,26 @@ const authTmpl = `
         <button onclick="oAuth2();"> 連結到 LineNotify 按鈕 </button>
 	</body>
 `
+const authTmp2 = `
+<!DOCTYPE html>
+<html lang="tw">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script>
+        function oAuth2() {
+            var URL = 'https://notify-bot.line.me/oauth/authorize?';
+            URL += 'response_type=code';
+            URL += '&client_id={{.ClientID}}';
+            URL += '&redirect_uri={{.CallbackURL}}';
+            URL += '&scope=notify';
+            URL += '&state=NO_STATE';
+            window.location.href = URL;
+        }
+    </script>
+    </head>
+    <body>
+        <button onclick="oAuth2();"> 連結到 LineNotify 按鈕 </button>
+	</body>
+`

e098f7f7795ea44db450b2312c3bba160aef0920
gary97129 <79617638+gary97129@users.noreply.github.com>
2021-12-24 12:29:53 +0800

    Update tmpl.go

--- tmpl.go
+++ tmpl.go
@@ -24,18 +24,4 @@ const authTmpl = `
         <button onclick="oAuth2();"> 連結到 LineNotify 按鈕 </button>
 	</body>
 `
-func authTmp2(byt []byte) string{
-	const html = fmt.Sprintf(`
-	<!DOCTYPE html>
-	<html lang="tw">
-	    <head>
-		<title>token</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-	    </head>
-	    <body>
-		<h1> %s </h1>
-	    </body>
-	`,strings(byt))
-	return html
-}
+

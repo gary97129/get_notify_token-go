bba4ac4e28e3eef932774eca236d0b44be474b40
gary97129 <79617638+gary97129@users.noreply.github.com>
2021-12-24 12:21:57 +0800

    Update tmpl.go

--- tmpl.go
+++ tmpl.go
@@ -1,4 +1,5 @@
 package main
+import "fmt"
 
 const authTmpl = `
 <!DOCTYPE html>
@@ -23,26 +24,16 @@ const authTmpl = `
         <button onclick="oAuth2();"> 連結到 LineNotify 按鈕 </button>
 	</body>
 `
-const authTmp2 = `
-<!DOCTYPE html>
-<html lang="tw">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script>
-        function oAuth2() {
-            var URL = 'https://notify-bot.line.me/oauth/authorize?';
-            URL += 'response_type=code';
-            URL += '&client_id={{.ClientID}}';
-            URL += '&redirect_uri={{.CallbackURL}}';
-            URL += '&scope=notify';
-            URL += '&state=NO_STATE';
-            window.location.href = URL;
-        }
-    </script>
-    </head>
-    <body>
-        <button onclick="oAuth2();"> 連結到 LineNotify 按鈕 </button>
-	</body>
-`
+func String2Bytes(byt []byte) {
+	const authTmp2 = fmt.Sprintf(`
+	<!DOCTYPE html>
+	<html lang="tw">
+	    <head>
+		<title>token</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	    </head>
+	    <body>
+		<h1> %s </h1>
+	    </body>
+	`,strings(byt))

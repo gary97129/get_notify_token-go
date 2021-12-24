124e25f472a48a6cdd54d01a2ed6cb15c1fe0d69
gary97129 <79617638+gary97129@users.noreply.github.com>
2021-12-24 12:04:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	res := newTokenResponse(byt)
 	fmt.Println("result:", res)
 	token = res.AccessToken
-	byt = "11111"
+	byt = []byte("11111111111")
 	w.Write(byt)
 }
 func authHandler(w http.ResponseWriter, r *http.Request) {
